51e30608bf226a4a3734b9b716df90b2f66c7b7f
nicktest701 <nicktest701@gmail.com>
2023-07-11 00:16:45 +0000

    configure db

--- config/knex.js
+++ config/knex.js
@@ -5,13 +5,24 @@
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    // host: 'db4free.net',
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     port: 3306,
-     user: 'nicktest701',
-    password: 'Akwasi21@guy',
-    database: 'trupaddy',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
 });
+
+// const knex = require('knex')({
+//   client: 'mysql',
+//   connection: {
+//     // host: 'db4free.net',
+//     host: '127.0.0.1',
+//     port: 3306,
+//      user: 'nicktest701',
+//     password: 'Akwasi21@guy',
+//     database: 'trupaddy',
+//   },
+// });
 
 module.exports = knex;
